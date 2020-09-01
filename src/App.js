84726c27b360cd83612f3739f61966024b60dbd5
toshi0819 <toshitaka2732b@gmail.com>
2020-09-01 23:16:16 +0900

    create-components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React from 'react';
 
-class App extends React.Component {
-  render(){
-    const gree = "Hi, Tom!";
-    const aree = " nice to meet you";
-    const dom = <h1>{gree + aree}</h1>
+//class App extends React.Component {
+//  render(){
+//    const gree = "Hi, Tom!";
+//    const aree = " nice to meet you";
+//    const dom = <h1>{gree + aree}</h1>
+//  return (
+//    <React.Fragment>
+//      <label htmlFor="bar">bar</label>
+//      <input type="text" onChange={() => { console.log("I'm Changed.") }} />
+//    </React.Fragment>
+//    
+//  );
+//  }
+//}
+
+const App = () => {
   return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => { console.log("I'm Changed.") }} />
-    </React.Fragment>
-    
+    <div>
+      <Cat/>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
   );
-  }
+}
+
+const Cat = () => {
+  return <div>Meow</div>
 }
 
 export default App;
